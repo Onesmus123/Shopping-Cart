c3e48a8db11c84932df0e2e3c332bc9edd156605
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2025-07-22 16:17:29 +0300

    Updated navbar

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// main.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("nav ul");
+
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  }
+});
+
+
 let carts = document.querySelectorAll('.add-cart');
 
 // Product Data Array
