7f49d6ed2937cf0c57c1bbc50ab644edc7960dd6
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2025-07-23 07:38:31 +0300

    Updated Carousel

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if (hamburger && navMenu) {
     hamburger.addEventListener("click", () => {
       navMenu.classList.toggle("active");
+
     });
   }
 });
@@ -210,5 +211,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
